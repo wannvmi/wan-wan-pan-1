944e59021eda259d7e4156bf3ab46f168bee834e
wannvmi <wannvmi@163.com>
2021-01-11 00:07:09 +0800

    增加首页

---
+++ src/WanWan.Pan.App/Common/Converters/IUrlToBitmapConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using WanWan.Pan.App.Helpers;
+
+namespace WanWan.Pan.App.Common.Converters
+{
+    /// <summary>
+    /// 地址转图片转换器
+    /// </summary>
+    public class IUrlToBitmapConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                string fileurl = $"{AppDomain.CurrentDomain.BaseDirectory}Skin\\Kind\\{value.ToString()}";
+                if (File.Exists(fileurl))
+                {
+                    BitmapImage fileImg = ImageHelper.ConvertToImage(fileurl);
+                    return fileImg;
+                }
+            }
+            BitmapImage img = ImageHelper.ConvertToImage($"{AppDomain.CurrentDomain.BaseDirectory}Images\\background.png");
+            return img;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/WanWan.Pan.App/Helpers/ImageHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WanWan.Pan.App.Helpers
+{
+    /// <summary>
+    /// 图标操作类
+    /// </summary>
+    public class ImageHelper
+    {
+        public static BitmapImage ConvertToImage(string fileName)
+        {
+            BitmapImage bmp = new BitmapImage();
+            bmp.BeginInit();
+            bmp.UriSource = new System.Uri(fileName);
+            bmp.EndInit();
+            bmp.Freeze();
+            return bmp;
+        }
+    }
+}
